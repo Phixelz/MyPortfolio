d4d2f4a0f9445d205f897bed10fd365f7f357fac
Charleston Amaral <charleston.silva03@gmail.com>
2021-10-09 11:37:16 -0300

    chore: added responsive menu and hero component creation

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,3 +1,6 @@
+import Link from 'next/link'
+import { FiInstagram, FiGithub, FiLinkedin } from 'react-icons/fi'
+
 import styles from './styles.module.scss'
 
 export function Hero() {
@@ -5,7 +8,37 @@ export function Hero() {
     <section className={styles.wrapper}>
       <h2><span>Eu sou Phillipi,</span> User Experience Analyst Pleno.</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. A adipiscing ac porta egestas.</p>
-        <button type="button">Download Resume</button>
+      <button type="button">Download Resume</button>
+
+      <ul className="social">
+        <li>
+          <Link href="#">
+            <a>
+              <FiInstagram />
+            </a>
+          </Link>
+        </li>
+
+        <li>
+          <Link href="#">
+            <a>
+              <FiGithub />
+            </a>
+          </Link>
+        </li>
+
+        <li>
+          <Link href="#">
+            <a>
+              <FiLinkedin />
+            </a>
+          </Link>
+        </li>
+      </ul>
+
+      <ul className="email">
+        <li>phillipi.alves@gmail.com</li>
+      </ul>
     </section>
     
   )
