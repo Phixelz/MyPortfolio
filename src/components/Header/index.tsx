23ac55bfe830845229e5fd24e8fe1eb751f8cef7
Phillipi Alves <phixelz@gmail.com>
2021-10-10 16:55:46 -0300

    update build

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -15,60 +15,6 @@ export function Header() {
     <header>
       <div className={styles.wrapper}>
         <h1>Phillipi Alves.</h1>
-
-        <button type="button" onClick={handleToggleMenu}>
-          {hidden ? <FiMenu /> : <FiX />}
-        </button>
-
-        <nav className={styles.navigation} aria-hidden={hidden.toString()}>
-          <ul className="nav" onClick={handleToggleMenu}>
-            <li>
-              <Link href="/#about">
-                <a>Sobre</a>
-              </Link>
-            </li>
-
-            <li>
-              <Link href="/#projects">
-                <a>Projetos</a>
-              </Link>
-            </li>
-
-            <li>
-              <Link href="/#contact">
-                <a>Contato</a>
-              </Link>
-            </li>
-          </ul>
-
-          <button type="button" className="download">Download Resume</button>
-
-          <ul className="social">
-            <li>
-              <Link href="#">
-                <a>
-                  <FiInstagram />
-                </a>
-              </Link>
-            </li>
-
-            <li>
-              <Link href="#">
-                <a>
-                  <FiGithub />
-                </a>
-              </Link>
-            </li>
-
-            <li>
-              <Link href="#">
-                <a>
-                  <FiLinkedin />
-                </a>
-              </Link>
-            </li>
-          </ul>
-        </nav>
       </div>
     </header>
   )
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -9,36 +9,6 @@ export function Hero() {
       <h2><span>Eu sou Phillipi,</span> User Experience Analyst Pleno.</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. A adipiscing ac porta egestas.</p>
       <button type="button">Download Resume</button>
-
-      <ul className="social">
-        <li>
-          <Link href="#">
-            <a>
-              <FiInstagram />
-            </a>
-          </Link>
-        </li>
-
-        <li>
-          <Link href="#">
-            <a>
-              <FiGithub />
-            </a>
-          </Link>
-        </li>
-
-        <li>
-          <Link href="#">
-            <a>
-              <FiLinkedin />
-            </a>
-          </Link>
-        </li>
-      </ul>
-
-      <ul className="email">
-        <li>phillipi.alves@gmail.com</li>
-      </ul>
     </section>
     
   )
