1e9f466a7d39b22842686a84acd28dba2d63ffb0
Phillipi Alves <phixelz@gmail.com>
2021-10-12 18:02:36 -0300

    update build

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,12 +1,9 @@
-import Link from 'next/link'
-import { FiInstagram, FiGithub, FiLinkedin } from 'react-icons/fi'
-
 import styles from './styles.module.scss'
 
 export function Hero() {
   return (
     <section className={styles.wrapper}>
-      <h2><span>Eu sou Phillipi,</span> User Experience Analyst Pleno.</h2>
+      <h2><span>Eu sou Phillipi,</span> User Experience Analyst Pleno | SPC Brasil.</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. A adipiscing ac porta egestas.</p>
       <button type="button">Download Resume</button>
     </section>
