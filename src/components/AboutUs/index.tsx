35a48354f98b9953b001f73551ab57262032d7dc
Phillipi Alves <phixelz@gmail.com>
2021-12-05 23:34:37 -0300

    update aboutus

--- src/components/AboutUs/index.tsx
+++ src/components/AboutUs/index.tsx
@@ -8,29 +8,29 @@ export function AboutUs() {
       <Title
         title="Sobre mim"
         subtitle="01. Experiência"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. A adipiscing ac porta egestas. Turpis viverra egestas arcu pulv inar bibendum."
+        description="UX Designer autodidata com 5 anos de experiência criando projetos para startups e empresas de médio e grande porte."
       />
 
       <ul>
         <li>
           <h3>UX Designer Pleno - SPC Brasil</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. A adipiscing ac porta egestas. Turpis viverra egestas arcu pulv inar bibendum.</p>
+          <p>Abr 2021 - Atual</p>
         </li>
         <li>
           <h3>UX/UI Designer - Mundo Wap</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. A adipiscing ac porta egestas. Turpis viverra egestas arcu pulv inar bibendum.</p>
+          <p>Abr 2019 - Dez 2020 -  </p>
         </li>
         <li>
           <h3>UX/UI Designer - G-Digital</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. A adipiscing ac porta egestas. Turpis viverra egestas arcu pulv inar bibendum.</p>
+          <p>Abr 2019 - Jun 2019</p>
         </li>
         <li>
           <h3>UI Designer - Amigoo</h3>
-          <p>Contratação freelancer para criação de interface de formulário com foco em conversão.</p>
+          <p>Aug 2019 - Aug 2019</p>
         </li>
         <li>
           <h3>Full Stack Designer - Midhaz</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. A adipiscing ac porta egestas. Turpis viverra egestas arcu pulv inar bibendum.</p>
+          <p>May 2018 - Dez 2018</p>
         </li>
       </ul>
     </section>
