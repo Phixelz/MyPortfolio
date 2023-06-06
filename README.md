5702f4b1633da0f02acbdba1c8a2798ee8024be8
Phillipi Alves <phixelz@gmail.com>
2023-06-06 16:47:18 -0300

    update README

---
+++ README.md
@@ -0,0 +1,14 @@
+![App Cover](.github/cover.png)
+
+# Portfolio
+
+My Portfolio
+
+## Stacks
+
+- SCSS
+- TypeScript
+
+## Libs
+
+- Styled Components
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,27 +1,28 @@
-import { useCallback } from 'react'
+import { useCallback } from "react";
 
-import { FiArrowUp, FiHeart } from 'react-icons/fi'
+import { FiArrowUp, FiHeart } from "react-icons/fi";
 
-import styles from './styles.module.scss'
+import styles from "./styles.module.scss";
 
 export function Footer() {
   const handleScrollToTop = useCallback(() => {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth'
-    })
-  }, [])
+      behavior: "smooth",
+    });
+  }, []);
 
   return (
     <footer className={styles.wrapper}>
       <hgroup>
         <h2>Phillipi Alves.</h2>
-        <p className={styles.iconheart}>Desenvolvido com <FiHeart/> por mim
+        <p className={styles.iconheart}>
+          Desenvolvido com <FiHeart /> por mim
         </p>
       </hgroup>
       <button type="button" onClick={handleScrollToTop}>
         <FiArrowUp />
       </button>
     </footer>
-  )
+  );
 }
