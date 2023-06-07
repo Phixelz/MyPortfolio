a452a74d8e0397392993a69a88cf9989a3cdbb60
Phillipi Alves <phixelz@gmail.com>
2023-06-07 18:17:44 -0300

    components update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,14 @@
-import Link from 'next/link'
-import { useCallback, useState } from 'react'
-import { FiMenu, FiX, FiInstagram, FiGithub, FiLinkedin } from 'react-icons/fi'
-
-import styles from './styles.module.scss'
+import styles from "./styles.module.scss";
 
 export function Header() {
-  const [hidden, setHidden] = useState(false);
-
-  const handleToggleMenu = useCallback(() => {
-    setHidden((state) => !state);
-  }, [])
-
   return (
-    <header>
+    <header className={styles.container}>
+      <hgroup>
       <div className={styles.wrapper}>
-        <h1>Phillipi Alves.</h1>
+        <h1>PhillipiAlves.</h1>
+        <h1>PT-BR</h1>
       </div>
+      </hgroup>
     </header>
-  )
+  );
 }
